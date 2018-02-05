b845411b117cbf03e5e09ff23b536b2b9cef6f16
Nuno Miguel Silva <nuno.miguel.silva@ua.pt>
2018-02-05 00:08:15 +0000

    yaw roll and pitch calculated 📐 nearest green point to the camera obtained 🍏

--- src/jaco_kinect_calibration_node.cpp
+++ src/jaco_kinect_calibration_node.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+
 // PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
@@ -8,21 +9,27 @@
 #include <sensor_msgs/point_cloud2_iterator.h>
 
 #include <stdio.h>
-#include <math.h>       /* fmod */
+#include <math.h>
 #include <opencv2/opencv.hpp>
 
 #include <iostream>
 #include <sstream>
+#include <string>
 #include <stdint.h>
 
+# define M_PI           3.14159265358979323846  /* pi */
 
 using namespace sensor_msgs;
 using namespace cv;
 using namespace std;
 
-ros::Publisher pub;
+ros::Publisher pubgreen;
+ros::Publisher pubpaper;
 bool rotation_set = false;
-int p1x,p1y,p1z,p2x,p2y,p2z,p3x,p3y,p3z;
+int calib_rot_count = 0;
+float p1x,p1y,p1z,p2x,p2y,p2z,p3x,p3y,p3z;
+float p1xm,p1ym,p1zm,p2xm,p2ym,p2zm,p3xm,p3ym,p3zm;
+double yaw,pitch,roll;
 
 int getH(int r, int g, int b){
 
@@ -146,6 +153,10 @@ int getV(int r, int g, int b){
 }
 
 void filtergreen(const sensor_msgs::PointCloud2Ptr& input){
+
+    bool greendetect[input -> height * input -> width];
+    bool green_todelete[input -> height * input -> width];
+
     for (int i=0; i < input -> height; i++)
     {
         for (int j=0; j < input -> width; j++)
@@ -156,14 +167,7 @@ void filtergreen(const sensor_msgs::PointCloud2Ptr& input){
 
             unsigned char *pt;
 
-            //unsigned int p = input -> data + input -> row_step*i + j*input -> point_step;
-
             pt = (input -> data).data() + input -> row_step*i + j*input -> point_step;
-
-            //cout << (int)input -> data[input -> row_step*i + j*input -> point_step] << endl;
-            //cout << (int)input -> data[input -> row_step*i + j*input -> point_step+1] << endl;
-            //cout << (int)input -> data[input -> row_step*i + j*input -> point_step+2] << endl;
-            //cout << (int)input -> data[input -> row_step*i + j*input -> point_step+3] << endl;
 
             memcpy(&x,pt,4);
 
@@ -187,18 +191,14 @@ void filtergreen(const sensor_msgs::PointCloud2Ptr& input){
             s = getS(r,g,b);
             v = getV(r,g,b);
 
-            //cout << " r:" << r << " g: " << g << " b: " << b << endl;
-            //cout << " h:" << h << " s: " << s << " v: " << v << endl;
-
-
             if(h >=70 && h <=100 && s >= 36 && s <= 115 && v >= 92 && v <= 255){ //green HSV values
-                /*
-                input -> data[input -> row_step*i + j*input -> point_step + 16] = 0;
-                input -> data[input -> row_step*i + j*input -> point_step + 17] = 0;
-                input -> data[input -> row_step*i + j*input -> point_step + 18] = 255;
-                */
+
+                greendetect[i*640+j] = true;
+
             }
             else{ // send them into the void
+
+                greendetect[i*640+j] = false;
 
                 //x nan
                 input -> data[input -> row_step*i + j*input -> point_step] = 0;
@@ -217,14 +217,139 @@ void filtergreen(const sensor_msgs::PointCloud2Ptr& input){
                 input -> data[input -> row_step*i + j*input -> point_step + 11] = 127;
 
             }
-
-            //cout << "i:" << i << " j:" << j << endl;
-            //cout << "x:" << x << " y:" << y << " z:" << z << endl;
-            //cout << "r:" << r << " g:" << g << " b:" << b << endl;
-
-            //cout << input -> data[input -> row_step*i + j*input -> point_step] << endl;
         }
     }
+
+    //detect unsignificant points
+    for (int i=0; i < input -> height; i++) {
+        for (int j = 0; j < input->width; j++) {
+            if(greendetect[i*640+j] == 1){
+                if(!(j==0 | j==479 | i%480==0 | i % 480 == 479)){ // not in border
+                    if(greendetect[i*640+j-1] == false ) green_todelete[i*640+j] = true; // left
+                    else if (greendetect[i*640+j+1] == false ) green_todelete[i*640+j] = true; // right
+                    else if (greendetect[i*640+j-input -> width] == false ) green_todelete[i*640+j] = true; // up
+                    else if (greendetect[i*640+j+input -> width] == false ) green_todelete[i*640+j] = true; // down
+                    else green_todelete[i*640+j] = false;
+                }
+            }
+
+        }
+    }
+
+    // SEND THEM TO THE VOID
+    for (int i=0; i < input -> height; i++) {
+        for (int j = 0; j < input->width; j++) {
+            if((j==0 | j==479 | i%480==0 | i % 480 == 479 | green_todelete[i*640+j])){
+                //x nan
+                input -> data[input -> row_step*i + j*input -> point_step] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 1] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 2] = 192;
+                input -> data[input -> row_step*i + j*input -> point_step + 3] = 127;
+                //y nan
+                input -> data[input -> row_step*i + j*input -> point_step + 4] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 5] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 6] = 192;
+                input -> data[input -> row_step*i + j*input -> point_step + 7] = 127;
+                //z nan
+                input -> data[input -> row_step*i + j*input -> point_step + 8] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 9] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 10] = 192;
+                input -> data[input -> row_step*i + j*input -> point_step + 11] = 127;
+            }
+
+        }
+    }
+
+    //
+
+    double r;
+    double d = 9999;
+    float gx,gy,gz;
+
+    //get nearest point
+    for (int i=0; i < input -> height; i++) {
+        for (int j = 0; j < input->width; j++) {
+
+            float x, y, z;
+            x = y = z = 0;
+
+            unsigned char *pt;
+
+            pt = (input->data).data() + input->row_step * i + j * input->point_step;
+
+            memcpy(&x, pt, 4);
+
+            memcpy(&y, pt + 4, 4);
+
+            memcpy(&z, pt + 8, 4);
+
+            r = sqrt(pow(x,2)+pow(y,2)+pow(z,2));
+
+            if(d > r){
+                d = r;
+                gx = x;
+                gy = y;
+                gz = z;
+            }
+        }
+    }
+
+    cout << "gx: " << gx << ", gy: " << gy << ", gz: " << gz << endl;
+
+    // stay with the nearest point
+    // delete the others
+    for (int i=0; i < input -> height; i++) {
+        for (int j = 0; j < input->width; j++) {
+
+            float x, y, z;
+            x = y = z = 0;
+
+            unsigned char *pt;
+
+            pt = (input->data).data() + input->row_step * i + j * input->point_step;
+
+            memcpy(&x, pt, 4);
+
+            memcpy(&y, pt + 4, 4);
+
+            memcpy(&z, pt + 8, 4);
+
+            // SEND THEM TO THE VOID
+            if(x!=gx | y!=gy | z!=gz){
+                //x nan
+                input -> data[input -> row_step*i + j*input -> point_step] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 1] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 2] = 192;
+                input -> data[input -> row_step*i + j*input -> point_step + 3] = 127;
+                //y nan
+                input -> data[input -> row_step*i + j*input -> point_step + 4] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 5] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 6] = 192;
+                input -> data[input -> row_step*i + j*input -> point_step + 7] = 127;
+                //z nan
+                input -> data[input -> row_step*i + j*input -> point_step + 8] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 9] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 10] = 192;
+                input -> data[input -> row_step*i + j*input -> point_step + 11] = 127;
+            }
+        }
+    }
+
+
+    //check is green point is below the paper, if yes flip 180 degrees
+    if(gz < p1zm){
+        pitch = M_PI;
+        cout << "PITCH: " << M_PI << endl;
+    }
+    else{
+        cout << "PITCH: 0.0" << endl;
+        pitch = 0;
+    }
+
+    //from this point on, calculation have to be done with the rotation done
+    // TODO: this
+    //system("rosrun tf static_transform_publisher 0 0 0 0 "+ yaw +" "+ pitch +" "+ roll +" m1n6s200_link_base camera_rgb_optical_frame 100");
+
 }
 
 void filterpaper(const sensor_msgs::PointCloud2Ptr& input){
@@ -354,6 +479,151 @@ void filterpaper(const sensor_msgs::PointCloud2Ptr& input){
         }
     }
 
+    //extract p1, p2 and p3.
+    float xmin,xmax,ymax;
+    xmin = 9999.0;
+    xmax = ymax = -9999.0;
+
+    for (int i=0; i < input -> height; i++) {
+        for (int j = 0; j < input -> width; j++) {
+
+            float x,y,z;
+            x = y = z = 0;
+
+            unsigned char *pt;
+
+            pt = (input -> data).data() + input -> row_step*i + j*input -> point_step;
+
+            memcpy(&x,pt,4);
+
+            memcpy(&y,pt+4,4);
+
+            memcpy(&z,pt+8,4);
+
+            // watch out for opposite coordinate X!=Y
+            if(xmin > x){ //p1
+                xmin = x;
+                p1x = x;
+                p1y = y;
+                p1z = z;
+            }
+
+            if(ymax < y){ //p2
+                ymax = y;
+                p2x = x;
+                p2y = y;
+                p2z = z;
+            }
+
+            if(xmax < x){ //p3
+                xmax = x;
+                p3x = x;
+                p3y = y;
+                p3z = z;
+            }
+
+        }
+    }
+
+    /*
+    cout << "PAPER POINT COORDS: " << endl;
+    cout << "p1:(" << p1x << ", " << p1y << ", " << p1z << ")" << endl;
+    cout << "p2:(" << p2x << ", " << p2y << ", " << p2z << ")" << endl;
+    cout << "p3:(" << p3x << ", " << p3y << ", " << p3z << ")\n" << endl;
+    */
+
+    // SEND THEM TO THE VOID
+    for (int i=0; i < input -> height; i++) {
+        for (int j = 0; j < input->width; j++) {
+
+            float x,y,z;
+            x = y = z = 0;
+
+            unsigned char *pt;
+
+            pt = (input -> data).data() + input -> row_step*i + j*input -> point_step;
+
+            memcpy(&x,pt,4);
+
+            memcpy(&y,pt+4,4);
+
+            memcpy(&z,pt+8,4);
+
+            if(!((x == p1x && y == p1y && z == p1z)|(x == p2x && y == p2y && z == p2z)|(x == p3x && y == p3y && z == p3z))){
+                //x nan
+                input -> data[input -> row_step*i + j*input -> point_step] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 1] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 2] = 192;
+                input -> data[input -> row_step*i + j*input -> point_step + 3] = 127;
+                //y nan
+                input -> data[input -> row_step*i + j*input -> point_step + 4] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 5] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 6] = 192;
+                input -> data[input -> row_step*i + j*input -> point_step + 7] = 127;
+                //z nan
+                input -> data[input -> row_step*i + j*input -> point_step + 8] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 9] = 0;
+                input -> data[input -> row_step*i + j*input -> point_step + 10] = 192;
+                input -> data[input -> row_step*i + j*input -> point_step + 11] = 127;
+            }
+            else{
+                if(x == p1x && y == p1y && z == p1z){ //p1
+                    input -> data[input -> row_step*i + j*input -> point_step + 16] = 0;
+                    input -> data[input -> row_step*i + j*input -> point_step + 17] = 0;
+                    input -> data[input -> row_step*i + j*input -> point_step + 18] = 255;
+                }
+                if(x == p2x && y == p2y && z == p2z){ //p2
+                    input -> data[input -> row_step*i + j*input -> point_step + 16] = 0;
+                    input -> data[input -> row_step*i + j*input -> point_step + 17] = 255;
+                    input -> data[input -> row_step*i + j*input -> point_step + 18] = 0;
+                }
+                if(x == p3x && y == p3y && z == p3z){ //p3
+                    input -> data[input -> row_step*i + j*input -> point_step + 16] = 255;
+                    input -> data[input -> row_step*i + j*input -> point_step + 17] = 0;
+                    input -> data[input -> row_step*i + j*input -> point_step + 18] = 0;
+                }
+            }
+        }
+    }
+
+    //after sending them to the void we calculate the average of 20 readings
+
+    calib_rot_count++;
+    if(calib_rot_count < 20){
+        p1xm = (p1xm*calib_rot_count+p1x)/(calib_rot_count+1);
+        p1ym = (p1ym*calib_rot_count+p1y)/(calib_rot_count+1);
+        p1zm = (p1zm*calib_rot_count+p1z)/(calib_rot_count+1);
+        p2xm = (p2xm*calib_rot_count+p2x)/(calib_rot_count+1);
+        p2ym = (p2ym*calib_rot_count+p2y)/(calib_rot_count+1);
+        p2zm = (p2zm*calib_rot_count+p2z)/(calib_rot_count+1);
+        p3xm = (p3xm*calib_rot_count+p3x)/(calib_rot_count+1);
+        p3ym = (p3ym*calib_rot_count+p3y)/(calib_rot_count+1);
+        p3zm = (p3zm*calib_rot_count+p3z)/(calib_rot_count+1);
+    }
+    else{
+        cout << "PAPER POINT MEAN COORDS: " << endl;
+        cout << "p1:(" << p1xm << ", " << p1ym << ", " << p1zm << ")" << endl;
+        cout << "p2:(" << p2xm << ", " << p2ym << ", " << p2zm << ")" << endl;
+        cout << "p3:(" << p3xm << ", " << p3ym << ", " << p3zm << ")\n" << endl;
+
+        double d,x;
+
+        //between p1 and p2
+        d = sqrt(pow((p2xm-p1xm),2)+pow((p2ym-p1ym),2));
+        x = abs(p1zm - p2zm);
+        roll = asin(x/d);
+        roll = roll*(M_PI/1.8);
+        cout << "roll: " << roll << endl;
+
+        //between p1 and p3
+        d = sqrt(pow((p3xm-p1xm),2)+pow((p3ym-p1ym),2));
+        x = abs(p1zm - p3zm);
+        yaw = asin(x/d);
+        yaw = yaw*(M_PI/1.8);
+        cout << "yaw: " << yaw << endl;
+    }
+
+
 }
 
 void cloud_cb (const sensor_msgs::PointCloud2Ptr& input){
@@ -379,9 +649,13 @@ void cloud_cb (const sensor_msgs::PointCloud2Ptr& input){
 
     output = *input;
 
-    //cout << output -> data[input -> row_step + input -> point_step + 11] << endl;
+    if(calib_rot_count >= 20) rotation_set = true;
 
-    pub.publish (output);
+    if(rotation_set) {
+        pubgreen.publish(output);
+    }else {
+        pubpaper.publish (output);
+    }
 }
 
 int main (int argc, char** argv){
@@ -389,11 +663,21 @@ int main (int argc, char** argv){
     ros::init (argc, argv, "jaco_kinect_calibration_node");
     ros::NodeHandle nh;
 
+    cout << "=====================================================" << endl;
+    cout << "|     KINECT CAMERA CALIBRATION USING JACO ARM      |" << endl;
+    cout << "| INTELLIGENT AND MOBILE ROBOTICS FINAL PROJECT BY: |" << endl;
+    cout << "|    Nuno Silva          &         Marcos Pires     |" << endl;
+    cout << "=====================================================\n" << endl;
+
+
     // Create a ROS subscriber for the input point cloud
     ros::Subscriber sub = nh.subscribe ("/camera/depth_registered/points", 1, cloud_cb);
 
     // Create a ROS publisher for the output point cloud
-    pub = nh.advertise<sensor_msgs::PointCloud2> ("/output/filtered_cloud", 1);
+    pubgreen = nh.advertise<sensor_msgs::PointCloud2> ("/output/filtered_cloud/greenpoints", 1);
+    pubpaper = nh.advertise<sensor_msgs::PointCloud2> ("/output/filtered_cloud/paperpoints", 1);
+
+
 
     // Spin
     ros::spin ();
